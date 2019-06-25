37c1067cb215816eb8b0b05833bdcff26fd5f03e
Harum Helmy <harumendhah@gmail.com>
2019-06-24 22:54:14 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+require "pry"
+
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+    array.sort_by { |x| x.length }
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse 
+end
+
+def kesha_maker(array)
+    array.each { |x| x.sub!(x[2], "$") }
+    array
+end
+
+def find_a(array)
+    array.select { |x| x.start_with?("a") }
+end
+
+def sum_array(array)
+    sum = 0 
+    array.each { |x| sum = sum + x } 
+    sum
+     # array.inject  { |sum, n| sum + n }
+end
+
+def add_s(array)
+    array.map { |x| 
+    if x != "feet"
+        x + "s"
+    else
+        "feet"
+    end
+}
+end
+
+
+
+
+
 
