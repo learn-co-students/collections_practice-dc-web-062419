e0f5b1ecbd84595229d3fca731ea1ca98c2fd17f
SkaMateria <skamateria@gmail.com>
2019-06-30 18:17:00 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+require 'pry'
+
+
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort_by {|char| char.length}
+end
+
+def swap_elements(array)
+    array.insert(1, array.delete_at(2))
+end
+
+def swap_elements_from_to(array, index, destination_index)
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|char| char[2] = "$"}
+end
+
+def find_a(array)
+    array.delete_if {|x| x[0] != "a"}
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    array.collect.with_index do |x, i|
+        if i == 1
+            x
+        else
+            x + "s"
+        end
+    end
+end
+
 
